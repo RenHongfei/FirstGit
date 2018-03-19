e4c0799376d152dfbff24edd86f77546fafff5d9
Hongfei Ren <214698225@qq.com>
2018-03-19 00:45:19 -0700

    1
    1

--- hello的副本.py
+++ hello的副本.py
@@ -1,17 +1,18 @@
-<<<<<<< HEAD
 
 创建分支
 print("Hello world")
-=======
 
 print ("I want to learn Python")
+<<<<<<< HEAD
 >>>>>>> parent of 37efd62... 第一次删除
 <<<<<<< HEAD
 =======
 + 一句话
 我想要这样改
 >>>>>>> New-Masterh
+=======
  parent of 37efd62... 第一次删除
 + 一句话
 我想要这样改
 
+>>>>>>> e09896021c9815c4414666c0196b34ecb3d8b472
